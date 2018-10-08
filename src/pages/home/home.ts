4220cb076055efe0ecbbe081a19b8e76746dd0eb
WarrenG8 <warrengyoung8@gmail.com>
2018-10-08 13:30:17 -0700

    Added todo list page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { TodoDirective } from '../../directives/todo/todo';
+import { Todo } from '../../directives/directives.module';
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [TodoDirective]
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  newTodo: Todo = new Todo();
 
+  constructor(public navCtrl: NavController, public _todoServe: TodoDirective) {
+
+  }
+
+  addTodo() {
+    if (!this.newTodo.title) {
+      return;
+    }
+
+    this._todoServe.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  toggleTodoComplete(todo) {
+    this._todoServe.toggleTodoComplete(todo);
+  }
+
+  removeTodo(todo) {
+    this._todoServe.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this._todoServe.getAllTodos();
   }
 
 }
